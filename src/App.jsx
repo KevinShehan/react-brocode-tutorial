60033f2bf9fabeb2bb4cc02143b24319b6fef0f8
Kevin <kevin.shehan30@gmail.com>
2024-03-07 20:46:53 +0530

    V1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Food from "./Food";
+import Header from "./Header";
+import Footer from "./Footer";
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Food />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+
+
+function Food() {
+    const food1="Rice";
+    const food2="Koththu";
+  return (
+    <div>
+        <ul>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Food
