1b5180d97309564d86ef76e41f7ec9cd84c5ba12
Kevin <kevin.shehan30@gmail.com>
2024-03-08 12:58:46 +0530

    Button modular css file create

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,13 @@ import Food from "./Food";
 import Header from "./Header";
 import Footer from "./Footer";
 import Card from "./Card";
-
+import Button from "./Button/Button";
 
 function App() {
 
-
   return (
     <>
-     <Card/>
-     <Card/>
-     <Card/>
-     <Card/>
-     <Card/>
-     <Card/>
+      <Button />
     </>
   )
 }
