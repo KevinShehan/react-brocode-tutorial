5d793ba0be6303a40fb0819f3edade99548f8cc7
Kevin <kevin.shehan30@gmail.com>
2024-03-08 14:35:28 +0530

    inline CSS

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import './Button.modular.css';
+
+// import './Button.modular.css';
+
 
 function Button() {
+
+  const styles = { 
+    backgroundColor:" hsl(200, 100%, 50%)",
+    padding: "10px 20px",
+    color: "white",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+  };
   return (
     <div>
-        <button className='button'>CLick me</button>
+        <button style={styles} className="button">CLick me</button>
     </div>
   )
 }
