69b2b3ac9ce6597fcd982544b5d4d13a57dc4c3e
Kevin <kevin.shehan30@gmail.com>
2024-03-08 10:25:50 +0530

    card images css aded

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,11 @@
 import Img1 from "./283441.png"
+
 function Card() {
     return (
         <div className="card">
-            <img src={Img1} alt="Profile Picture" />
-            <h2>Kevin Code</h2>
-            <p>Kevin Shehan Perera</p>
+            <img className="card-image" src={Img1} alt="Profile Picture" />
+            <h2 className="card-title">Kevin Code</h2>
+            <p className="card-text">Kevin Shehan Perera</p>
         </div>
     )
 }
